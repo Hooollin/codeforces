4f1fa771e82055b8b2e4e0b03a5f07ba0f3b6584
Hollin <hollinisme@gmail.com>
2021-08-09 17:48:35 +0800

    solved with tutorial

--- div2/735/B.cpp
+++ div2/735/B.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,6 +12,17 @@ int main(){
     while(T-- > 0){
         int n, k;
         cin >> n >> k;
+        vector<int> arr(n + 1);
+        for(int i = 1; i <= n; i++){
+            cin >> arr[i];
+        }
+        long long ans = INT_MIN;
+        for(int i = max(1, n - 2 * k); i <= n; i++){
+            for(int j = i + 1; j <= n; j++){
+                ans = max(1LL * i * j - 1LL * k * (arr[i] | arr[j]), ans);
+            }
+        }
+        cout << ans << endl;
     }
     return 0;
 }
